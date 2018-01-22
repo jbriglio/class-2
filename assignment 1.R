a76957c99495f913147606ea7c5d47e37a18dc66
Janie Briglio <janiebriglio@dhcp-wifi-8021x-168-122-13-68.bu.edu>
2018-01-22 16:04:28 -0500

    homework

---
+++ assignment 1.R
@@ -0,0 +1,19 @@
+##Assignment 1
+#Question 1 
+#a 
+c(1:20)
+#b
+c(20:1)
+#c
+c(1:20, 19:1)
+#d
+c(4,6,3)->tmp 
+#e
+rep(tmp, times=10)
+#f
+rep(tmp, length=31)
+#g 
+x<-4
+y<-6 
+z <- 3
+#Question 2 
